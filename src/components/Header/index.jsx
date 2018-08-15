0a8aa34ed509c7d5e4319946b3ce903f132f55a4
Marcin Krawczyk <marckraw@icloud.com>
2018-08-15 17:00:44 +0200

    [374] Add arrow back to header when not on dashboard or main. Add helper function to display props in stateless components

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,26 +1,38 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import { withRouter } from 'react-router';
+
 import calendar from '../../assets/icons/calendar.svg';
 import avatar from '../../assets/avatar.png';
+import arrowLeft from '../../assets/icons/arrow_left.svg';
 
 import IconButton from 'components/IconButton';
 import Image from 'components/Image';
 
 import * as S from './styled.js';
 
-const Header = () => {
+const Header = props => {
     return (
         <S.Container>
-            <NavLink to={'/main/profile'}>
-                <Image
-                    src={avatar}
-                    alt="Logged in person profile picture"
-                    width="50"
-                    height="50"
-                    onClick={this.toggleLogin}
+            {props.location.pathname === '/main' ||
+            props.location.pathname === '/main/dashboard' ? (
+                <NavLink to={'/main/profile'}>
+                    <Image
+                        src={avatar}
+                        alt="Logged in person profile picture"
+                        width="50"
+                        height="50"
+                    />
+                </NavLink>
+            ) : (
+                <IconButton
+                    src={arrowLeft}
+                    alt="Go to calendar"
+                    height="50px"
+                    onClick={props.history.goBack}
                 />
-            </NavLink>
+            )}
             <S.MainHeading>daily advisor</S.MainHeading>
             <NavLink to={'/main/calendar'}>
                 <IconButton src={calendar} alt="Go to calendar" height="50px" width="50px" />
@@ -29,4 +41,4 @@ const Header = () => {
     );
 };
 
-export default Header;
+export default withRouter(Header);
--- src/components/IconButton/index.jsx
+++ src/components/IconButton/index.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Icon = styled.img`
-    width: ${props => (props.width ? props.width : '200px')};
-    height: ${props => (props.height ? props.height : '200px')};
+    width: ${props => (props.width ? props.width : null)};
+    height: ${props => (props.height ? props.height : null)};
 `;
 
 const IconButton = props => {
-    return <Icon width={props.width} height={props.height} src={props.src} alt={props.alt} />;
+    return (
+        <Icon
+            width={props.width}
+            height={props.height}
+            src={props.src}
+            alt={props.alt}
+            {...props}
+        />
+    );
 };
 
 export default IconButton;
