30428451ba79ac3ff539ea4a5af37cf95f74ea71
Marcin Krawczyk <marckraw@icloud.com>
2018-08-21 18:20:55 +0200

    [392] move mocked responses to other folder

---
+++ src/utils/mockedResponses/user.js
@@ -0,0 +1,15 @@
+export const getOwnProfile = {
+    status: 200,
+    statusText: 'OK',
+    data: {
+        about: 'Dreamer who loves tee shirts, travelling and meeting new people.',
+        city: 'Katowice',
+        lastName: 'Krawczyk',
+        name: 'Marcin',
+        dateOfBirth: '1991-02-15',
+        userId: '1',
+        userType: 'client',
+        email: 'marckraw@icloud.com',
+        profilePictureId: '12345678901234567890',
+    },
+};
